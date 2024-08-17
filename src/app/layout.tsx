d058865084b21ca66f4223eca08fbb763f0924fa
Acer <regina-sabitova-1998@mail.ru>
2024-08-18 00:42:55 +0500

    первый коммит

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["cyrillic"] });
+
+export const metadata: Metadata = {
+  title: "Skypro music",
+  description: "Слушай до дыр",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ru">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
