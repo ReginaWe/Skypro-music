1c359931645640a59c381e4e9831c68f30e42fd0
Acer <regina-sabitova-1998@mail.ru>
2024-08-18 23:45:29 +0500

    Продолжаю разбивать на компоненты

---
+++ src/app/components/Nav/Nav.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import styles from "./Nav.module.css"
+
+const Nav = () => {
+  return (
+    <nav className={styles.mainNav}>
+      <div className={styles.navLogo}>
+        <Image
+          className={styles.logoImage}
+          src="/img/logo.png"
+          alt="logo"
+          height={114}
+          width={17}
+        />
+      </div>
+      <div className={styles.navBurger}>
+        <span className={styles.burgerLine} />
+        <span className={styles.burgerLine} />
+        <span className={styles.burgerLine} />
+      </div>
+      <div className={styles.navMenu}>
+        <ul className={styles.menuList}>
+          <li className={styles.menuItem}>
+            <a href="#" className={styles.menuLink}>
+              Главное
+            </a>
+          </li>
+          <li className={styles.menuItem}>
+            <a href="#" className={styles.menuLink}>
+              Мой плейлист
+            </a>
+          </li>
+          <li className={styles.menuItem}>
+            <a href="../signin.html" className={styles.menuLink}>
+              Войти
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
