143495c54485c5a7991d32304b84366c4fc5a885
Acer <regina-sabitova-1998@mail.ru>
2024-09-11 22:33:07 +0500

    Настройки Redux и ReduxProvider

---
+++ src/store/features/playlistSlice.ts
@@ -0,0 +1,23 @@
+import { TrackType } from "@/app/types/tracks";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type PlaylistStateType =  {
+  currentTrack: null | TrackType;
+}
+
+const initialState: PlaylistStateType = {
+    currentTrack: null,
+};
+
+const playlistSlice = createSlice({
+  name: "playlist",
+  initialState,
+  reducers: {
+    setCurrentTrack: (state, action: PayloadAction<TrackType>) => {
+      state.currentTrack = action.payload;
+    },
+  },
+});
+
+export const { setCurrentTrack } = playlistSlice.actions;
+export const playlistReducer = playlistSlice.reducer;
